8513335d1740f2307ce39b330ca2885b62fc8ab3
quanganh3183 <quanganh3183@gmail.com>
2025-03-11 22:05:48 +0700

    Update out-order.table.jsx

--- FE/src/components/out-order/out-order.table.jsx
+++ FE/src/components/out-order/out-order.table.jsx
@@ -15,9 +15,6 @@ import { useNavigate } from "react-router-dom";
 
 
 
-
-
-
 const OutOrderTable = (props) => {
   const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
   const [dataUpdate, setDataUpdate] = useState({});
